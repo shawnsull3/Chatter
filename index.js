b86f952ce846d8dc8aaf8b166e918f5793b2c4b7
Shawn Sullivan <sullivansm3@gmail.com>
2020-04-27 09:50:26 -0600

    Render and emit chat messages to all clients

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var app = require('express')();
-var http = require('http').createServer(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
